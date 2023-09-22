f6c4e4710c17b1af5bad77a967807ffb3e1257c8
Muqaddas Rashid <muqaddasrashid@mac.local>
2023-09-22 23:56:28 +0500

    REST API routes added

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+//middleware
+app.use(express.json())
+
+//routes
+app.get('/hello', (req, res) => {
+  res.send('Task Manager App');
+})
+
+app.use('/api/v1/tasks', tasks)
+
+// // get all the tasks
+// app.get('/api/v1/tasks')
+
+// // create a new tasks
+// app.post('/api/v1/tasks')
+
+// // Get single task
+// app.get('/api/v1/tasks/:id')
+
+// // update a task
+// app.patch('/api/v1/tasks/:id')
+
+// // delete task
+// app.delete('/api/v1/tasks/:id')
+
+
+const port = 3000;
+
+app.listen(port, console.log(`server is listening on port (${port})`));
