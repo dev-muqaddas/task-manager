043f36e334fb3f32d2d380e2d5982ace59740651
Muqaddas Rashid <muqaddasrashid@mac.local>
2023-09-23 15:47:09 +0500

    db connected task schema added and validations added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
 //middleware
 app.use(express.json())
@@ -12,22 +14,15 @@ app.get('/hello', (req, res) => {
 
 app.use('/api/v1/tasks', tasks)
 
-// // get all the tasks
-// app.get('/api/v1/tasks')
-
-// // create a new tasks
-// app.post('/api/v1/tasks')
-
-// // Get single task
-// app.get('/api/v1/tasks/:id')
-
-// // update a task
-// app.patch('/api/v1/tasks/:id')
-
-// // delete task
-// app.delete('/api/v1/tasks/:id')
-
 
 const port = 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`server is listening on port (${port})`));
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-app.listen(port, console.log(`server is listening on port (${port})`));
+start()
